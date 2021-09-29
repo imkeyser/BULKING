2d26334af7647df7ed1f0d2b771f40e6404c8993
Abeille <tabeille@albmolecular.com>
2021-09-29 16:11:01 +0200

    Upload project

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+$('document').ready(function(){
+    $('.calculsForm').hide();
+    $('#home-start-button').click(function(){
+        $('.left-block').fadeOut(100);
+        $('.calculsForm').fadeIn(900);
+    });
+    
+});
